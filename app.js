0dd7daab6a6d8c5b4350bcd0fb8e20865ed55ca7
Umer Farooq <64475488+umerfarooq8743@users.noreply.github.com>
2022-12-25 22:37:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const quizData=[
+    {
+        question:"Q1: What is the sum of 4 and 8?",
+        a:"12",
+        b:"17",
+        c:"23",
+        d:"03",
+        correct:"a",
+    }  ,
+{
+        question:"Q2: What is the sum of 5 and 2?",
+        a:"12",
+        b:"07",
+        c:"23",
+        d:"03",
+        correct:"b",
+    }   ,
+{
+        question:"Q3: What is the sum of 1 and 8?",
+        a:"12",
+        b:"17",
+        c:"23",
+        d:"09",
+        correct:"d",
+    }   ,
+{
+        question:"Q4: What is the sum of 5 and 8?",
+        a:"12",
+        b:"13",
+        c:"23",
+        d:"03",
+        correct:"b",
+    }
+];
+
+const quiz=document.getElementById('quiz');
+const answerEls= document.querySelectorAll('.answer');
+const questionEl= document.getElementById('question');
+const a_text= document.getElementById('a_text');
+const b_text= document.getElementById('b_text');
+const c_text= document.getElementById('c_text');
+const d_text= document.getElementById('d_text');
+const submitBtn=document.getElementById('submit');
+ 
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+function loadQuiz(){
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText=currentQuizData.question
+    a_text.innerText= currentQuizData.a
+    b_text.innerText= currentQuizData.b
+    c_text.innerText= currentQuizData.c
+    d_text.innerText= currentQuizData.d
+}
+
+function deselectAnswers(){
+    answerEls.forEach(answerEl => answerEl.checked=false)
+}
+
+function getSelected(){
+    let answer 
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked){
+            answer=answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', ()=>{
+    const answer=getSelected()
+    if(answer){
+        if (answer ===quizData[currentQuiz].correct){
+            score++ 
+        }
+        currentQuiz++
+        if(currentQuiz<quizData.length){
+            loadQuiz()
+        }else{
+            quiz.innerHTML=`<h2> your answered ${score}/${quizData.length}questions correctly </h2>
+            <button onclick="location.reload()"> Reload </button>"`
+        }
+    }
+})
